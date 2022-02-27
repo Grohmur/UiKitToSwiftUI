3e721ad0cf63faf7d4fb3c5555e99b27483cc786
Grohmur <zverma92@gmail.com>
2022-02-27 10:29:12 +0300

    Initial Commit

---
+++ UiKitToSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UiKitToSwiftUI
+//
+//  Created by Михаил Зверьков on 27.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UiKitToSwiftUI/UiKitToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiKitToSwiftUIApp.swift
+//  UiKitToSwiftUI
+//
+//  Created by Михаил Зверьков on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct UiKitToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
